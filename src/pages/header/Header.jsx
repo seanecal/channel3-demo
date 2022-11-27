c33d0ab638b90ffa430ba12777daec27e2855956
Sen Cao <caosen.cn@gmail.com>
2022-11-27 23:43:43 +0200

    added text animations to the header section: '

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -4,6 +4,9 @@ import butterfly_left from '../../assets/logo-left.png'
 import butterfly_right from '../../assets/logo-right.png'
 import hero from '../../assets/c3-hero.png'
 import line from '../../assets/line.png'
+import text from '../../assets/hero-text.png'
+import d from '../../assets/d-text.png'
+import n from '../../assets/n-text.png'
 
 export function Header() {
   return(
@@ -11,6 +14,9 @@ export function Header() {
       <Navbar />
       <div className="w3__header-container">
         <div className="w3__header-leftcontainer">
+          <div className="w3__hader-left__text">
+            <img src={text} alt="" />
+          </div>
           <div className="w3__header-left">
             <div className='w3__header-left__left'>
               <img src={butterfly_left} alt="" />
@@ -24,6 +30,12 @@ export function Header() {
           </button>
         </div>
         <div className="w3__header-right">
+          <div className="w3__header-right-decentralized">
+            <img src={d} alt="decentralized" />
+          </div>
+          <div className="w3__header-right-news">
+            <img src={n} alt="news are the news from the people" />
+          </div>
           <div className="w3__header-right__hero">
             <img src={hero} alt="a man recording with his phone" />
           </div>
